236dca5a8f16e5ff7ce677633281e2598cbbbc94
BasedOnTechnology <therobotist@gmail.com>
2021-04-14 01:07:44 -0400

    Some work on the paper wording

--- gekko_optimal_control.py
+++ gekko_optimal_control.py
@@ -20,14 +20,19 @@ H(s)/R= X(s) / F(s) = (R*a1 + R*a2 s + R*a3 s^2) / (b1 s + b2 s^2 + b3 s^3)
 "Solution: Separate the equation so that the output terms, X(s), are on the
 left and the input terms, Fa(s), are on the right.  Make sure there are only positive powers of s."
 
-(R*a1 + R*a2 s + R*a3 s^2) = (b1 s + b2 s^2 + b3 s^3)
+(R*a1 + R*a2 s + R*a3 s^2) X(s) = (b1 s + b2 s^2 + b3 s^3) U(s)
 
 "Now take the inverse Laplace Transform (so multiplications by "s" in the Laplace domain are replaced by derivatives in time)."
 
-(R*a1 x + R*a2 x' + R*a3 x'') = (b1 f' + b2 f'' + b3 f''')
+(R*a1 x + R*a2 x' + R*a3 x'') = (b1 u' + b2 u'' + b3 u''')
+
+where u is the input function,
 
 now, gekko wants it in first order, so we have to substitute:
 http://www.sharetechnote.com/html/DE_HigherOrderDEtoFirstOrderDE.html
+https://math.stackexchange.com/questions/1120984/
+
+https://math.berkeley.edu/~zworski/128/psol12.pdf
 
 
 
@@ -48,6 +53,11 @@ final = m.Param(value=p)
 # Equations
 m.Equation(x1.dt()==u)
 m.Equation(x2.dt()==0.5*x1**2)
+
+integral()
+abs2()
+
+
 m.Obj(x2*final) # Objective function
 m.options.IMODE = 6 # optimal control mode
 m.solve(disp=False) # solve
