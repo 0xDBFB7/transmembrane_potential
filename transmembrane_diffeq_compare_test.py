e3334ba3dfcaee05184d2a6b5c5162e35d24590d
BasedOnTechnology <therobotist@gmail.com>
2021-05-08 15:23:12 -0400

    Different

--- transmembrane_diffeq_compare_test.py
+++ transmembrane_diffeq_compare_test.py
@@ -23,12 +23,11 @@ m.time = np.linspace(t0, tstop/T0, nt)
 t1 = np.linspace(t0, tstop, nt)
 
 
-host_cell = Cell(0.3, 80, 0.3, 80, 1e-7, 5, 20e-6, 5e-9, t1 )
-virus = Cell(0.3, 80, 0.005, 30, 1e-8, 60, 50e-9, 14e-9, t1 )
+virus = Cell(np.float128(0.3), np.float128(80), np.float128(0.005), np.float128(30), np.float128(1e-8), np.float128(60), np.float128(50e-9), np.float128(14e-9), t1)
 
  # differential equation is wrong - must be just after gekko_.
 
-control_input = np.sin(t1/((end/30.0)/T0))
+control_input = np.sin(t1/((end/30.0)))
 
 
 
@@ -56,11 +55,6 @@ gamma_v = m.Const(virus.R*virus.a_3/virus.b_1)
 phi_v = m.Const(virus.b_2/virus.b_1)
 xi_v = m.Const(virus.b_3/virus.b_1)
 
-alpha_h = m.Const(host_cell.R*host_cell.a_1/host_cell.b_1)
-beta_h = m.Const(host_cell.R*host_cell.a_2/host_cell.b_1)
-gamma_h = m.Const(host_cell.R*host_cell.a_3/host_cell.b_1)
-phi_h = m.Const(host_cell.b_2/host_cell.b_1)
-xi_h = m.Const(host_cell.b_3/host_cell.b_1)
 
 SX0 = m.Const(X0)
 SU0 = m.Const(U0)
@@ -85,16 +79,13 @@ virus_output = np.array(x0_v.value)
 plt.figure(1) # plot results
 # plt.plot(m.time,x1.value,'k-',label=r'$x_1$')
 # plt.plot(m.time,x2.value,'b-',label=r'$x_2$')
-plt.plot(m.time*T0,virus_output*X0 / U0,'r',label=r'$x0_v$')
-# plt.plot(m.time*T0,host_output*X0 / U0,'b',label=r'$x0_h$')
-plt.legend(loc='best')
-plt.xlabel('Time')
-plt.ylabel('Value')
-plt.show()
-plt.figure(2) # plot results
-plt.plot( t1, convolve_output(control_input, virus, end/nt))
+integration_method = virus_output*X0 / U0
+plt.plot(m.time*T0,integration_method,'r',label=r'$x0_v$')
+convolution_method_output = convolve_output(control_input, virus, end/nt)
+plt.plot( t1, convolution_method_output)
 
-print(np.max((virus_output*X0 / U0) - control_input))
+
+print(np.mean(np.abs(integration_method - convolution_method_output))/(np.max(convolution_method_output)))
 
 
 plt.show()
