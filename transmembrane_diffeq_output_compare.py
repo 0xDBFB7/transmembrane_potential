Fixed scaling issue

transmembrane_diffeq_output_compare.py
@@ -57,8 +57,10 @@ virus_output = np.array(x0_v.value)
 
 plt.figure(1) # plot results
 integration_method = virus_output*X0 / U0
+# plt.plot(m.time*T0,,'r',marker='o')
 
 plt.plot(m.time*T0,integration_method,'r',marker='o', label="GEKKO integrator")
+# plt.plot(m.time*T0,x1_v.value,'g',marker='o')
 
 convolution_method_output = convolve_output(u_step, virus, end/nt)
 plt.plot(t1, convolution_method_output, label="lib_onvolution_method_output")
@@ -67,15 +69,25 @@ plt.plot(t1, convolution_method_output, label="lib_onvolution_method_output")
 plt.plot(t1, virus.step_response.astype('float32',casting='same_kind'), marker='o', label='lib_step_response')
 
 system = signal.lti(np.float64([virus.a_3,virus.a_2,virus.a_1]),np.float64([virus.b_3,virus.b_2,virus.b_1]))
-#output is transfer function times input times R!
 
-_, yout, _ = signal.lsim2(system, U=np.float64(u_step), T=np.float64(t1),)
-plt.plot(t1, yout*virus.R, label='lsim2')
-_, yout, _ = signal.lsim(system, U=np.float64(u_step), T=np.float64(t1))
-plt.plot(t1, yout*virus.R, label='lsim')
-_, yout = signal.step(system, T=np.float64(t1))
-plt.plot(t1+end/2, yout*virus.R, label='scipy step')
+# _, yout, _ = signal.lsim2(system, U=np.float64(u_step), T=np.float64(t1)) # this isn't working for some reason
+# plt.plot(t1, yout, label='lsim2')
+# _, yout, _ = signal.lsim(system, U=np.float64(u_step), T=np.float64(t1))
+# plt.plot(t1, yout, label='lsim')
+# _, yout = signal.step(system, T=np.float64(t1))
+# plt.plot(t1+end/2, yout, label='scipy step')
 
 plt.legend()
 
 plt.show()
+
+#
+#
+#
+# from scipy.integrate import quad
+# def integrand(x, a, b):
+#     return a*x**2 + b
+#
+# a = 2
+# b = 1
+# I = quad(integrand, 0, 1, args=(a,b))

transmembrane_lib.py
@@ -73,7 +73,6 @@ class Cell:
         self.alpha = ((self.R*self.a_3/self.b_3))
         self.beta = ((self.R*self.a_2/self.b_3))
         self.gamma = ((self.R*self.a_1/self.b_3))
-        print(self.gamma, self.R,self.a_1,self.b_3)
         self.phi = ((self.b_2/self.b_3))
         self.xi = ((self.b_1/self.b_3))
 
@@ -241,3 +240,58 @@ def delta_transmembrane_trapezoid(t, t_start, t_rise, t_fall, duration, cell, E_
     # is the duration t1+rise perhaps?
 
     return out * E_field_magnitude
+
+
+class gekko_sim_method:
+    def __init__(self, control_input_u0, u1=None, u2=None, N=500, T0=1e-6, X0=1e-6, U0=0):
+        # U0 scaling is not really needed
+        self.m = GEKKO() # initialize gekko
+        self.m.options.MAX_ITER = 200
+        U0 = 1.0
+        X0 = 1e-6
+        end = tstop / T0
+        self.m.time = np.linspace(0,end,N)
+
+        # control_input_u0 =
+        # gekko_input = np.float64(interpolant(np.float64(m.time)*T0))
+        # u0 = m.Param(value=gekko_input)
+        # u1_ = np.diff(gekko_input, prepend=gekko_input[0])/((end/nt))
+        # u1 = m.Param(value=u1_)
+        # u2_ = np.diff(u1_, prepend=u1_[0])/((end/nt)/T0)
+        # u2 = m.Param(value=u2_)
+
+        self.x0_v = m.Var(value=0)
+        self.x1_v = m.Var(value=0)
+        self.x2_v = m.Var(value=0)
+
+        self.x0_h = m.Var(value=0)
+        self.x1_h = m.Var(value=0)
+        self.x2_h = m.Var(value=0)
+
+        alpha_h = m.Const(host_cell.alpha)
+        beta_h = m.Const(host_cell.beta)
+        gamma_h = m.Const(host_cell.gamma)
+        phi_h = m.Const(host_cell.phi)
+        xi_h = m.Const(host_cell.xi)
+
+        alpha_v = m.Const(virus.alpha)
+        beta_v = m.Const(virus.beta)
+        gamma_v = m.Const(virus.gamma)
+        phi_v = m.Const(virus.phi)
+        xi_v = m.Const(virus.xi)
+
+        SX0 = m.Const(X0)
+        SU0 = m.Const(U0)
+        ST0 = m.Const(T0)
+
+        m.Equation(self.x1_v==self.x0_v.dt())
+        m.Equation(self.x2_v==self.x1_v.dt())
+        m.Equation(self.x2_v == ((SU0 / (ST0**2))*alpha_v*u2 + (SU0 / ST0)*beta_v*u1 + gamma_v*SU0*u0 - phi_v*(SX0 / ST0)*self.x1_v - xi_v*SX0*self.x0_v)/(SX0 / (ST0**2)))
+
+        m.Equation(self.x1_h==self.x0_h.dt())
+        m.Equation(self.x2_h==self.x1_h.dt())
+        m.Equation(self.x2_h == ((SU0 / (ST0**2))*alpha_h*u2 + (SU0 / ST0)*beta_h*u1 + gamma_h*SU0*u0 - phi_h*(SX0 / ST0)*self.x1_h - xi_h*SX0*self.x0_h)/(SX0 / (ST0**2)))
+
+        m.options.IMODE = 4 # dynamic simulation
+
+        m.solve(disp=True) # solve

transmembrane_test_solution.py
@@ -19,79 +19,21 @@ T0 = 1e-8
 
 sim_t = np.loadtxt( 'PSOPT/build/t.dat', dtype=np.float128) * T0
 sim_t = sim_t[:-1] # duplicate!
+sim_u0 = np.loadtxt( 'PSOPT/build/u0.dat', dtype=np.float128 )[:-1]
 
-t0 = 0
-tstop = sim_t[-1]
-print(tstop)
-dt = 0.00005e-9
-t = np.linspace(t0, tstop, int(tstop/dt), dtype=np.float128)
+# t0 = 0
+# tstop = sim_t[-1]
+# print(tstop)
+# dt = 0.0005e-9 / T0
+# t = np.linspace(t0, tstop, int(tstop/dt), dtype=np.float128)
 
-# dt = (t[1] - t[0])
-
-sim_values = np.loadtxt( 'PSOPT/build/u0.dat', dtype=np.float128 )[:-1]
-
-
-# flinear = interpolate.interp1d(x, sim_values)
-# fcubic = interpolate.interp1d(sim_t, sim_values, kind="quadratic")
 interpolant = interpolate.InterpolatedUnivariateSpline(sim_t, sim_values, k=5)
-
 control_input = interpolant(np.float64(t))
-host_cell = default_host_cell(t)
-virus = default_virus(t)
+
+host_cell = default_host_cell(sim_t * T0)
+virus = default_virus(sim_t * T0)
 
 
-m = GEKKO() # initialize gekko
-m.options.MAX_ITER = 200
-nt = 3000
-U0 = 1.0
-X0 = 1e-6
-end = tstop / T0
-m.time = np.linspace(0,end,nt)
-
-gekko_input = np.float64(interpolant(np.float64(m.time)*T0))
-u0 = m.Param(value=gekko_input)
-u1_ = np.diff(gekko_input, prepend=gekko_input[0])/((end/nt)/T0)
-u1 = m.Param(value=u1_)
-u2_ = np.diff(u1_, prepend=u1_[0])/((end/nt)/T0)
-u2 = m.Param(value=u2_)
-
-x0_v = m.Var(value=0)
-x1_v = m.Var(value=0)
-x2_v = m.Var(value=0)
-
-x0_h = m.Var(value=0)
-x1_h = m.Var(value=0)
-x2_h = m.Var(value=0)
-
-# t = m.Param(value=m.time)
-
-alpha_h = m.Const(host_cell.alpha)
-beta_h = m.Const(host_cell.beta)
-gamma_h = m.Const(host_cell.gamma)
-phi_h = m.Const(host_cell.phi)
-xi_h = m.Const(host_cell.xi)
-
-alpha_v = m.Const(virus.alpha)
-beta_v = m.Const(virus.beta)
-gamma_v = m.Const(virus.gamma)
-phi_v = m.Const(virus.phi)
-xi_v = m.Const(virus.xi)
-
-SX0 = m.Const(X0)
-SU0 = m.Const(U0)
-ST0 = m.Const(T0)
-
-m.Equation(x1_v==x0_v.dt())
-m.Equation(x2_v==x1_v.dt())
-m.Equation(x2_v == ((SU0 / (ST0**2))*alpha_v*u2 + (SU0 / ST0)*beta_v*u1 + gamma_v*SU0*u0 - phi_v*(SX0 / ST0)*x1_v - xi_v*SX0*x0_v)/(SX0 / (ST0**2)))
-
-m.Equation(x1_h==x0_h.dt())
-m.Equation(x2_h==x1_h.dt())
-m.Equation(x2_h == ((SU0 / (ST0**2))*alpha_h*u2 + (SU0 / ST0)*beta_h*u1 + gamma_h*SU0*u0 - phi_h*(SX0 / ST0)*x1_h - xi_h*SX0*x0_h)/(SX0 / (ST0**2)))
-
-m.options.IMODE = 4 # dynamic simulation
-
-m.solve(disp=True) # solve
 
 
 