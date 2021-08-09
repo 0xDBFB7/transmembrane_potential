e73536bad25df670e45bde18fa3d8530b04cd977
BasedOnTechnology <therobotist@gmail.com>
2021-08-09 11:32:51 -0400

    Precisely the same problem in both implementations

--- nagurka_fourier_method/nagurka_fourier.jl
+++ nagurka_fourier_method/nagurka_fourier.jl
@@ -1,6 +1,6 @@
 
-using Test, ForwardDiff;
-
+using Test, ForwardDiff, NumericalIntegration;
+using Plots;
 
 
 epsilon = 1e-15
@@ -13,52 +13,108 @@ epsilon = 1e-15
 
 #const fdcache = ForwardDiffCache()
 
+#maybe try sin
 
 
 function P_coefficients(X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf, t_f, a, b, M)
     m = 1:M
-    @show m
-    @show sum(m.*a)
 
     tau = t_f
     p0 = X_t0 - sum(a)
     pf = X_tf - sum(a)
 
-    return p0, pf #d_p0, d_pf, d_d_p0, d_d_pf
+    #originally + here!
+    d_p0 = d_X_t0 - 2.0*sum(m.*((b.*pi)/t_f)) #reversed  sign fixed polynomial BC test?!?
+    d_pf = d_X_tf - 2.0*sum(m.*((b.*pi)/t_f)) # what on earth
+
+    d_d_p0 = d_d_X_t0 + 4.0 * sum(a.*(m.^2).*((pi^2) / (t_f^2)))
+    d_d_pf = d_d_X_tf + 4.0 * sum(a.*(m.^2).*((pi^2) / (t_f^2)))
+
+    return (p0, pf, d_p0, d_pf, d_d_p0, d_d_pf)
 end
 
-@testset "Setup" begin
+function L_(t, a, b, M, t_f)
+    m = 1:M
+    return sum(a.*cos.(2*pi*(m*t)/t_f)) + sum(b.*sin.(2*pi*(m*t)/t_f))
+end;
+
+d_L_(t, a, b, M, t_f) = ForwardDiff.derivative(n -> L_(n, a, b, M, t_f), t)
+d_d_L_(t, a, b, M, t_f) = ForwardDiff.derivative(n -> d_L_(n, a, b, M, t_f), t)
+
+function P_(t, p, a, b, M, t_f)
+    p0, pf, d_p0, d_pf, d_d_p0, d_d_pf = p
+
+    tau = t/t_f
+
+    P = (-6.0*(p0 - pf) - 3.0*(d_p0 + d_pf)*t_f - (0.5*(d_d_p0 - d_d_pf)*(t_f^2)))*(tau^5)
+    P += (15*(p0 - pf) + (8*d_p0 + 7*d_pf)*t_f + ((3/2.0)*d_d_p0 - d_d_pf)*(t_f^2))*(tau^4)
+    P += (-10*(p0 - pf) - (6.0*d_p0 + 4.0*d_pf)*t_f - 0.5*(3.0*d_d_p0 - d_d_pf)*(t_f^2))*(tau^3)
+    P += (0.5*d_d_p0*(t_f^2))*(tau^2) + (d_p0*t_f)*tau + p0
+
+    return P
+end
+
+d_P_(t, p, a, b, M, t_f) = ForwardDiff.derivative(n -> P_(n, p, a, b, M, t_f), t)
+d_d_P_(t, p, a, b, M, t_f) = ForwardDiff.derivative(n -> d_P_(n, p, a, b, M, t_f), t)
+
+
+function X_(t,p,a,b,M,t_f)
+    return P_(t, p, a, b, M, t_f) + L_(t, a, b, M, t_f)
+end
+
+function d_X_(t,p,a,b,M,t_f)
+    return d_P_(t, p, a, b, M, t_f) + d_L_(t, a, b, M, t_f)
+end
+
+function d_d_X_(t,p,a,b,M,t_f)
+    return d_d_P_(t, p, a, b, M, t_f) + d_d_L_(t, a, b, M, t_f)
+end
+
+
+@testset "P_coefficients" begin
 
     t_f = 2.0
 
-    a = [-1.95643e-3]
-    b = [1.442172e-3]
+    a = [1.0]
+    b = [-2.0]
     M = 1
 
     t = range(epsilon,stop=t_f,length=100)
 
     X_t0 = 1.0 # is it possible that this is overconstrained?
     X_tf = 2.0
-    d_X_t0 = 0
+    d_X_t0 = -2.0
     d_X_tf = 3.0
-    d_d_X_t0 = 0
+    d_d_X_t0 = 6
     d_d_X_tf = -3.0
 
+    # @testset "Coefficient BCs" begin
+    #     p0, pf = P_coefficients(X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf, t_f, a, b, M)
+    #     @test isapprox(p0, X_t0, atol=1e-8, rtol=1e-8)
+    #     @test isapprox(pf, X_tf, atol=1e-8, rtol=1e-8)
+    # end;
 
-    @testset "Coefficient BCs" begin
+    @testset "Polynomial BCs" begin
 
-        p0, pf = P_coefficients(X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf, t_f, a, b, M)
-        @test isapprox(p0, X_t0, atol=1e-8, rtol=1e-8)
-        @test isapprox(pf, X_tf, atol=1e-8, rtol=1e-8)
+        p = P_coefficients(X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf, t_f, a, b, M)
+        P(n) = P_(n, p, a, b, M, t_f)
+        d_P(n) = d_P_(n, p, a, b, M, t_f)
+        d_d_P(n) = d_d_P_(n, p, a, b, M, t_f)
 
 
-    end;
+        @test isapprox(P(epsilon), X_t0 - L_(epsilon,a,b,M,t_f), atol=1e-8, rtol=1e-8)
+        @test isapprox(P(t_f), X_tf - L_(t_f,a,b,M,t_f), atol=1e-8, rtol=1e-8)
 
-end;
+        # this appears to be what's broken.
+        @test isapprox(d_P(epsilon), d_X_t0 - d_L_(epsilon,a,b,M,t_f), atol=1e-8, rtol=1e-8)
+        @test isapprox(d_P(t_f), d_X_tf - d_L_(t_f,a,b,M,t_f), atol=1e-8, rtol=1e-8)
 
-function L_(t, a, b, M, t_f)
-    m = 1:M
-    return sum(a.*cos.(2*pi*(m*t)/t_f)) + sum(b.*sin.(2*pi*(m*t)/t_f))
+        @test isapprox(d_d_P(epsilon), d_d_X_t0 - d_d_L_(epsilon,a,b,M,t_f), atol=1e-8, rtol=1e-8)
+        @test isapprox(d_d_P(t_f), d_d_X_tf - d_d_L_(t_f,a,b,M,t_f), atol=1e-8, rtol=1e-8)
+
+
+    end
+
 end;
 
 @testset "L_" begin
@@ -70,8 +126,6 @@ end;
     M = 2
 
     t = range(epsilon,stop=t_f,length=100)
-
-    d_L_(t, a, b, M, t_f) = ForwardDiff.derivative(n -> L_(n, a, b, M, t_f), t)
 
     v = (2*pi/t_f)
 
@@ -89,91 +143,87 @@ end;
 
     #
     t_ = [0.5, 2.0]
-    correct = -1.0*v*sin.(v*t_) + 2.0*v*2.0*sin.(2.0*t_*v) + -1.0*v*cos.(t_*v) + 3.0*v*2.0*cos.(2.0*t_*v)
-    @show correct
     d_L(t) = d_L_(t, a, b, M, t_f)
-    @test isapprox(d_L.(t_), correct, atol=1e-8, rtol=1e-8)
+    @test isapprox(d_L.(t_), d_L_compare.(t_), atol=1e-8, rtol=1e-8)
 
 
 end;
 
 
+function Kirk_A_optimal(t)
+    return 7.289.*t.-6.103.+6.696*exp.(-t).-0.593*exp.(t)
+end
+#
+function d_Kirk_A_optimal(t)
+    return 7.289 -6.696*exp.(-t)-0.593*exp.(t)
+end
+
+function d_d_Kirk_A_optimal(t)
+    # not discussed in the paper - just a guess
+    return 6.696*exp.(-t)-0.593*exp.(t)
+end
 
 
-# function P_(t)
-#
-#     P = (-6 * (p_0 - p_f)
-#     return
-# end
+@testset "Kirk_A_example" begin
+    """
+    Equation 32, Nagurka&Yen 1990, Case A
+    """
+
+    t_f = 2.0
+    a = [-1.95643e-3]
+    b = [1.442172e-3]
+
+    M = 1
+    t = range(epsilon, stop=t_f, length=60)
+
+    X_t0 = 0.0
+    X_tf = 5.0
+    d_X_t0 = 0.0
+    d_X_tf = 2.0
+    d_d_X_t0 = 6.1025137
+    d_d_X_tf = -3.4798053
 
 
-#
-#
-#
-# function Kirk_A_optimal(t)
-#     return 7.289*t -6.103 + 6.696*exp(-t)-0.593*exp(t)
-# end
-#
-# def d_Kirk_A_optimal(t):
-#     return 7.289 -6.696*exp(-t)-0.593*exp(t)
-#
-# def d_d_Kirk_A_optimal(t):
-#     # not discussed in the paper - just a guess
-#     return 6.696*exp(-t)-0.593*exp(t)
-#
-#
-# @testset "Kirk_A_example" begin
-#     """
-#     Equation 32, Nagurka&Yen 1990, Case A
-#     """
-#
-#     t_f = 2.0
-#     a = np.array([-1.95643e-3])
-#     b = np.array([1.442172e-3])
-#
-#     M = a.shape[0]
-#
-#     t = range(epsilon, t_f, 60)
-#
-#     X_t0 = 0.0
-#     X_tf = 5.0
-#     d_X_t0 = 0.0
-#     d_X_tf = 2.0
-#     d_d_X_t0 = 6.1025137
-#     d_d_X_tf = -3.4798053
-#
-#
-#     p = P_coefficients(X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf, t_f, a, b, M)
-#
-#     X = X_(t,p,a,b,M,t_f)
-#     d_X = d_X_(t,p,a,b,M,t_f)
-#     d_d_X = d_d_X_(t,p,a,b,M,t_f)
-#
-#     U = X + d_d_X
-#
-#     # all of these are perfect except U, need asserts
-#     # plt.plot(t,X)
-#     # plt.plot(t,P_(t, p, M))
-#     # plt.plot(t,
-#     # plt.plot(t[:-1],np.diff(X)/(1/30))
-#     # plt.plot(t,d_X)
-#     # plt.show()
-#     # plt.plot(t[:-2],np.diff(np.diff(X))/((1/30)**2.0))
-#     # plt.plot(t,d_d_X)
-#     # plt.plot(t,Kirk_A_optimal(t))
-#     # plt.plot(t,d_Kirk_A_optimal(t))
-#     # plt.plot(t,d_d_Kirk_A_optimal(t))
-#     # plt.plot(t,U)
-#     # plt.show()
-#
-#     # this test fails. the derivatives all work out perfectly, it's just the
-#     # U control has a completely different shape as fig 1a 1990.
-#     # must be misunderstanding the U completely somehow.
-#
-#     assert np.allclose(X,Kirk_A_optimal(t),rtol=0.004)
-#     assert np.allclose(d_X,d_Kirk_A_optimal(t),rtol=0.004)
-#     assert np.allclose(d_d_X,d_d_Kirk_A_optimal(t),rtol=0.004)
-#
-#     J = 0.5*integrate.simpson(U**2.0,t)
-#     assert J == pytest.approx(1.675e1)
-# end
+    p = P_coefficients(X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf, t_f, a, b, M)
+
+    _X_(n) = X_(n,p,a,b,M,t_f)
+    _d_X_(n) = d_X_(n,p,a,b,M,t_f)
+    _d_d_X_(n) = d_d_X_(n,p,a,b,M,t_f)
+    X = _X_.(t)
+    d_X = _d_X_.(t)
+    d_d_X = _d_d_X_.(t)
+
+    U = X + d_d_X
+
+    plot(t,X, show=true)
+
+    plot(t,U, show=true)
+
+    # all of these are perfect except U, need asserts
+    # plt.plot(t,X)
+    # plt.plot(t,P_(t, p, M))
+    # plt.plot(t,
+    # plt.plot(t[:-1],np.diff(X)/(1/30))
+    # plt.plot(t,d_X)
+    # plt.show()
+    # plt.plot(t[:-2],np.diff(np.diff(X))/((1/30)**2.0))
+    # plt.plot(t,d_d_X)
+    # plt.plot(t,Kirk_A_optimal(t))
+    # plt.plot(t,d_Kirk_A_optimal(t))
+    # plt.plot(t,d_d_Kirk_A_optimal(t))
+    # plt.plot(t,U)
+    # plt.show()
+
+    # this test fails. the derivatives all work out perfectly, it's just the
+    # U control has a completely different shape as fig 1a 1990.
+    # must be misunderstanding the U completely somehow.
+
+    @test isapprox(X,Kirk_A_optimal(t), atol=1e-8, rtol=1e-8)
+    @test isapprox(d_X,d_d_Kirk_A_optimal(t), atol=1e-8, rtol=1e-8)
+    @test isapprox(d_d_X,d_d_Kirk_A_optimal(t), atol=1e-8, rtol=1e-8)
+
+
+
+    J = 0.5*integrate(t, U.*U, SimpsonEven())
+    @test isapprox(J, 1.675e1, atol=1e-8, rtol=1e-8)
+end
