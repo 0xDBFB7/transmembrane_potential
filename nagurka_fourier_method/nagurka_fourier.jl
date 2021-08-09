bd3041fc5d206c7030f145724332077c4d89c7c9
BasedOnTechnology <therobotist@gmail.com>
2021-08-09 12:05:37 -0400

    Trying yet another re-statement

--- nagurka_fourier_method/nagurka_fourier.jl
+++ nagurka_fourier_method/nagurka_fourier.jl
@@ -16,43 +16,76 @@ epsilon = 1e-15
 #maybe try sin
 
 
+# function P_coefficients(X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf, t_f, a, b, M)
+#     m = 1:M
+#
+#     tau = t_f
+#     p0 = X_t0 - sum(a)
+#     pf = X_tf - sum(a)
+#
+#     #originally + here!
+#     d_p0 = d_X_t0 + 2.0*sum(m.*((b.*pi)/t_f)) #reversed  sign fixed polynomial BC test?!?
+#     d_pf = d_X_tf + 2.0*sum(m.*((b.*pi)/t_f)) # what on earth
+#
+#     d_d_p0 = d_d_X_t0 + 4.0 * sum(a.*(m.^2).*((pi^2) / (t_f^2)))
+#     d_d_pf = d_d_X_tf + 4.0 * sum(a.*(m.^2).*((pi^2) / (t_f^2)))
+#
+#     return (p0, pf, d_p0, d_pf, d_d_p0, d_d_pf)
+# end
+
 function P_coefficients(X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf, t_f, a, b, M)
-    m = 1:M
-
-    tau = t_f
-    p0 = X_t0 - sum(a)
-    pf = X_tf - sum(a)
-
-    #originally + here!
-    d_p0 = d_X_t0 - 2.0*sum(m.*((b.*pi)/t_f)) #reversed  sign fixed polynomial BC test?!?
-    d_pf = d_X_tf - 2.0*sum(m.*((b.*pi)/t_f)) # what on earth
-
-    d_d_p0 = d_d_X_t0 + 4.0 * sum(a.*(m.^2).*((pi^2) / (t_f^2)))
-    d_d_pf = d_d_X_tf + 4.0 * sum(a.*(m.^2).*((pi^2) / (t_f^2)))
-
-    return (p0, pf, d_p0, d_pf, d_d_p0, d_d_pf)
+    return (X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf)
 end
 
+
+# function L_(t, a, b, M, t_f)
+#     m = 1:M
+#     return sum(a.*cos.(2*pi*(m*t)/t_f)) + sum(b.*sin.(2*pi*(m*t)/t_f))
+# end;
+
 function L_(t, a, b, M, t_f)
+    # m = [1.0:M;]
     m = 1:M
-    return sum(a.*cos.(2*pi*(m*t)/t_f)) + sum(b.*sin.(2*pi*(m*t)/t_f))
+    alpha_k = -1 .+ 4(m.^2.0)*(pi^2)*((t/t_f)^2 - (2*(t/t_f)^3) + (t/t_f)^4) .+ cos.(2*pi*(m.*t)/t_f)
+    beta_k = 2.0*m.*pi*(-(t/t_f) + 10*(t/t_f)^3 - 15*(t/t_f)^4 + 6*(t/t_f)^5) .+ sin.(2*pi*(m.*t)/t_f)
+    return sum(a*alpha_k) + sum(b*beta_k)
+
 end;
 
 d_L_(t, a, b, M, t_f) = ForwardDiff.derivative(n -> L_(n, a, b, M, t_f), t)
 d_d_L_(t, a, b, M, t_f) = ForwardDiff.derivative(n -> d_L_(n, a, b, M, t_f), t)
 
+# function P_(t, p, a, b, M, t_f)
+#     p0, pf, d_p0, d_pf, d_d_p0, d_d_pf = p
+#
+#     tau = t/t_f
+#
+#     P = (-6.0*(p0 - pf) - 3.0*(d_p0 + d_pf)*t_f - (0.5*(d_d_p0 - d_d_pf)*(t_f^2)))*(tau^5)
+#     P += (15*(p0 - pf) + (8*d_p0 + 7*d_pf)*t_f + ((3/2.0)*d_d_p0 - d_d_pf)*(t_f^2))*(tau^4)
+#     P += (-10*(p0 - pf) - (6.0*d_p0 + 4.0*d_pf)*t_f - 0.5*(3.0*d_d_p0 - d_d_pf)*(t_f^2))*(tau^3)
+#     P += (0.5*d_d_p0*(t_f^2))*(tau^2) + (d_p0*t_f)*tau + p0
+#
+#     return P
+# end
+
 function P_(t, p, a, b, M, t_f)
-    p0, pf, d_p0, d_pf, d_d_p0, d_d_pf = p
+    X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf = p
 
     tau = t/t_f
 
-    P = (-6.0*(p0 - pf) - 3.0*(d_p0 + d_pf)*t_f - (0.5*(d_d_p0 - d_d_pf)*(t_f^2)))*(tau^5)
-    P += (15*(p0 - pf) + (8*d_p0 + 7*d_pf)*t_f + ((3/2.0)*d_d_p0 - d_d_pf)*(t_f^2))*(tau^4)
-    P += (-10*(p0 - pf) - (6.0*d_p0 + 4.0*d_pf)*t_f - 0.5*(3.0*d_d_p0 - d_d_pf)*(t_f^2))*(tau^3)
-    P += (0.5*d_d_p0*(t_f^2))*(tau^2) + (d_p0*t_f)*tau + p0
+    p0 = X_t0 + X_t0*t + (-10*X_t0 - 6*d_X_t0*t_f)*(tau)^3
+            + (15*X_t0 + d_X_t0*t_f)*(tau)^4 + (-6*X_t0 - 3*d_X_t0*t_f)*(tau)^5
+
+    p1 = 0.5*(t_f^2)*((tau^2) - 3*tau^3 + 3*tau^4 - tau^5)
+    p2 = (10*tau^3 - 15*tau^4 + 6*tau^5)
+    p3 = t_f*(-4*tau^3 + 7*tau^4 - 3*tau^5)
+    p4 = 0.5*(t_f^2)*(tau^3 - 2*tau^4 + tau^5)
+
+    P = p0 + p1*X_t0 + p2*X_tf + p3*d_X_tf + p3 * d_d_X_tf
 
     return P
 end
+
 
 d_P_(t, p, a, b, M, t_f) = ForwardDiff.derivative(n -> P_(n, p, a, b, M, t_f), t)
 d_d_P_(t, p, a, b, M, t_f) = ForwardDiff.derivative(n -> d_P_(n, p, a, b, M, t_f), t)
