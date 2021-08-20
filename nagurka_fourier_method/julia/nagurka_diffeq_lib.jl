e75109965f6a9eb1aeca48541fe8a8c5a5b8a515
BasedOnTechnology <therobotist@gmail.com>
2021-08-20 16:59:01 -0400

    Added pore current term

--- nagurka_fourier_method/julia/nagurka_diffeq_lib.jl
+++ nagurka_fourier_method/julia/nagurka_diffeq_lib.jl
@@ -41,19 +41,25 @@ end
 Base.to_index(s::svars) = Int(s)+1
 Base.to_indices(I::Tuple) = (Int(I[1])+1, I[2])
 
-struct cell_struct # this darn redundancy needed because virus is global pystruct and slow mutable
+# this darn redundancy needed because virus is global pystruct and slow mutable
+struct cell_struct 
     alpha
     beta
     gamma
     phi
     xi
+    membrane_permittivity
+    membrane_thickness
+    cell_diameter
 end
 
 mutable struct transmembrane_params
     # this makes it hard to use DiffEqParamEstim, but I can't see a way to pass t_f to diffeq otherwise.
     cell_v
     cell_h
-    O
+    a
+    b
+    p
     t_f 
     M 
     m
@@ -78,9 +84,71 @@ end
 
 function d_pore_density(V_m, N, N0, alpha, q, V_ep)
     k = (V_m/V_ep)^2
-
     return alpha * exp(k) * (1.0 - (N/N0)*exp(-q*k))
 end;
+
+
+function dVdt_tm_potential_pore_current(V_m, N, cell)
+    @show (V_m, N, cell)
+    """
+    The term of the first derivative of the transmembrane potential
+    caused by the current flow through the 
+    """
+    F = 96485.332 # units?
+    T = 295.0
+    R = 8.314
+
+    r_m = 0.76e-9 # pore radius constant
+    pore_solution_conductivity = 13.0 * 0.1 # mS/cm to S/m
+    # where does 1.3 S/m come from? that's pretty high...
+    w0 = 2.65 # ?
+    n = 0.15
+    v_m = V_m * (F/(R*T))
+
+    i_ep_term_1 = (pi * (r_m^2) * pore_solution_conductivity * v_m * R * T) / (F * cell.membrane_thickness)
+
+    i_ep_term_2_divisor_1 = exp(v_m)*((w0*exp(w0-n*v_m) - (n*v_m)) / (w0 - (n*v_m)))
+    i_ep_term_2_divisor_2 = -((w0*exp(w0+n*v_m) + (n*v_m)) / (w0 + (n*v_m)))
+    i_ep_term_2 = exp(v_m - 1) / (i_ep_term_2_divisor_1 + i_ep_term_2_divisor_2)
+
+    @show i_ep_term_1 i_ep_term_2_divisor_1 i_ep_term_2_divisor_2
+    i_ep = i_ep_term_1 * i_ep_term_2
+    # I = C_m dv/dt
+    # dv_dt = I/C_m
+    # permittivity already has the eps0 in it 
+    A = 4*pi*(cell.cell_diameter/2)^2
+    C_m = cell.membrane_permittivity * A / cell.membrane_thickness 
+
+    # C_m and A should maybe go in transmembrane_lib
+
+    return i_ep * N / C_m
+
+end
+#=
+
+When is the irreversible regime reached?
+
+Retelj:
+
+> "If a pore density of N = 10^14 m^−2 was reached at the pole of a membrane (the point where it
+was the highest), the membrane was considered to be electroporated (threshold of significant, i.e., observable electroporation).
+This value was taken from the model of DeBruin and Krassowska [49], which compared simulations with the experiments of Hibino et al. [1]."
+
+Improved2002 suggest:
+
+"Irreversible breakdown and cell rupture would, therefore, be the predicted result, for pores exceeding
+the stability threshold radius r crit of 18 nm. In Fig. 1, both the peak energy and radius of the local maxima shift for a 0.2 V
+transmembrane potential. The critical radius for stability reduces to about 5.8 nm."
+
+> "First, as evident from Fig. 1, there is no barrier for 0.4 V. 
+However, from experimental data, much higher membrane voltages of about 
+1.0 V are required [28] for irreversible breakdown and membrane rupture."
+
+DeBruin Modeling1999a use a constant pore density of 0.76 nm, and state that:
+
+> "Including the effects of pore radius requires a substantial addition to the model that will be the subject of a future study."
+
+=#
 
 function transmembrane_diffeq(d,s,params::transmembrane_params,t)
     """
@@ -89,39 +157,59 @@ function transmembrane_diffeq(d,s,params::transmembrane_params,t)
     O is a vector of parameters.
     """
     
-    @timeit to "views" begin
     m = params.m
     M = params.M
     t_f = params.t_f
 
-
     @timeit to "us" begin
-        s[iu0] = U = X_(t,p,a,b,m,t_f)
-        s[iu1] = d_U = d_X_(t,p,a,b,m,t_f)
-        s[iu2] = d_d_U = d_d_X_(t,p,a,b,m,t_f)
+        s[iu0] = U = X_(t,params.p,params.a,params.b,m,t_f)
+        s[iu1] = d_U = d_X_(t,params.p,params.a,params.b,m,t_f)
+        s[iu2] = d_d_U = d_d_X_(t,params.p,params.a,params.b,m,t_f)
     end
 
     T0 = 1.0
     U0 = 1.0
     X0 = 1.0
+    
+    irreversible_threshold = 1e14
+    irreversible_threshold_sharpness = 2.0 # sharper values seem to cause instabilities
+    #=
+    
+    Added an exp limiter to the transmembrane voltage,
+    representing the current flow through the pores.
+    the exp limiter could also be moved to the pore density term.
 
-    second_derivative_eq(cell, s1, s0) = ((U0 / (T0*T0))*cell.alpha*d_d_U + (U0 / T0)*cell.beta*d_U 
-                                    + cell.gamma*U0*U - cell.phi*(X0 / T0)*s[s1] - cell.xi*X0*s[s0])/(X0 / (T0*T0))
+    I have now moved this to the pore density term. 
+
+    This is going against my own views on standardization. 
+    I should use the validated pore current equations.
+
+    The problem with putting an * exp_limiter(iN_v)
+    on the first derivative of transmembrane potential 
+    is that the potential never decreases again.
+
+    The sharp rise of N causes a huge numerical instability.
+    =#
+
+    second_derivative_eq(cell, s1, s0) = (((U0 / (T0*T0))*cell.alpha*d_d_U + (U0 / T0)*cell.beta*d_U 
+                                    + cell.gamma*U0*U - cell.phi*(X0 / T0)*s[s1] - cell.xi*X0*s[s0])/(X0 / (T0*T0)))
+
+    exp_limiter(iN) = (exp(-(s[iN] / irreversible_threshold)^irreversible_threshold_sharpness))
 
     @timeit to "diffeq" begin
-    d[ ix0_v ] = s[ix1_v]
+    d[ ix0_v ] = s[ix1_v] - dVdt_tm_potential_pore_current(s[ix0_v], s[iN_v], params.cell_v) 
     d[ ix1_v ] = second_derivative_eq(params.cell_v, ix1_v, ix0_v)
-    d[ ix0_h ] = s[ix1_h]
+
+    d[ ix0_h ] = s[ix1_h] - dVdt_tm_potential_pore_current(s[ix0_h], s[iN_h], params.cell_h) 
     d[ ix1_h ] = second_derivative_eq(params.cell_h, ix1_h, ix0_h)
     end
-
-
-    d[iN_v] = d_pore_density(s[ix0_v], s[iN_v], params.pore_N0, params.pore_alpha, params.pore_q, params.pore_V_ep)/T0
-
-    d[iN_h] = d_pore_density(s[ix0_h], s[iN_h], params.pore_N0, params.pore_alpha, params.pore_q, params.pore_V_ep)/T0
-
     
+    d[iN_v] = (d_pore_density(s[ix0_v], s[iN_v], params.pore_N0, params.pore_alpha, params.pore_q, params.pore_V_ep)/T0) * exp_limiter(iN_v)
+    d[iN_h] = (d_pore_density(s[ix0_h], s[iN_h], params.pore_N0, params.pore_alpha, params.pore_q, params.pore_V_ep)/T0) * exp_limiter(iN_h)
+    # @show s
+    # @show d
 
+    return
 end
 
 
--- nagurka_fourier_method/julia/nagurka_diffeq_lib_test.jl
+++ nagurka_fourier_method/julia/nagurka_diffeq_lib_test.jl
@@ -15,6 +15,12 @@ Profile.clear_malloc_data()
 https://github.com/JuliaApproximation/FastGaussQuadrature.jl apparently
 is better if you can chose where to sample the thing, like an odesolver lets you.
 
+
+Vsc shortcuts:
+tree: ctl 0
+editor: ctl 1
+term: ctl k
+close tab: ctl w
 =#
 
 @testset "Comparison" begin
@@ -25,11 +31,11 @@ is better if you can chose where to sample the thing, like an odesolver lets you
     t_f = 1e-6
 
     initial_state_variables = zeros(length(instances(svars)))
-    initial_state_variables[iN_v] = pore_N0
-    initial_state_variables[iN_h] = pore_N0
+    initial_state_variables[iN_v] = tl.pore_N0
+    initial_state_variables[iN_h] = tl.pore_N0
 
-    a = [0.0, 0.0, 1.0]
-    b = [0.0, 0.0, 0.1]
+    a = [0.0, 0.0, 1.0e7]
+    b = [0.0, 0.0, 0.1e7]
     # a = rand(M)
     # b = rand(M)
 
@@ -38,10 +44,7 @@ is better if you can chose where to sample the thing, like an odesolver lets you
 
 
 
-    a = @view params.O[1:M]
-    b = @view params.O[M+1:(2*M)]
-    c = @view params.O[(2*M)+1:(2*M)+6]
-
+    
     X_t0, d_X_t0, d_d_X_t0, X_tf, d_X_tf, d_d_X_tf = c
 
     X_t0 = 0.0 # override
@@ -52,32 +55,45 @@ is better if you can chose where to sample the thing, like an odesolver lets you
     p = P_BCs_to_p_coefficients(P_BCs, t_f)
 
     O = zeros((2*M)+6)
-    cell_v = cell_struct(virus.alpha, virus.beta,virus.gamma,virus.phi,virus.xi)
-    cell_h = cell_struct(host_cell.alpha, host_cell.beta,host_cell.gamma,host_cell.phi,host_cell.xi)
-    params = transmembrane_params(cell_v, cell_h, O, t_f, M, m, tl.pore_N0, tl.pore_alpha, tl.pore_q, tl.pore_V_ep)
-    params.O[1:M] = a
-    params.O[M+1:(2*M)] = b
-    params.O[(2*M)+1:(2*M)+6] = c
+
+    virus_membrane_thickness = 5e-9 # overriding temporarily!
+
+    cell_v = cell_struct(virus.alpha, virus.beta,virus.gamma,virus.phi,virus.xi,
+                virus.membrane_permittivity, virus_membrane_thickness, virus.cell_diameter)
+    cell_h = cell_struct(host_cell.alpha, host_cell.beta,host_cell.gamma,host_cell.phi,host_cell.xi,
+                                    host_cell.membrane_permittivity, host_cell.membrane_thickness, host_cell.cell_diameter)
+    params = transmembrane_params(cell_v, cell_h, a, b, p, t_f, M, m, tl.pore_N0, tl.pore_alpha, tl.pore_q, tl.pore_V_ep)
+    # params.O[1:M] = a
+    # params.O[M+1:(2*M)] = b
+    # params.O[(2*M)+1:(2*M)+6] = c
+
+    # params.a = O[1:M]
+    # params.b = O[M+1:(2*M)]
+    # params.c = O[(2*M)+1:(2*M)+6]
+
 
 
     tspan = (epsilon, 1e-6)
     prob = ODEProblem(transmembrane_diffeq,initial_state_variables,tspan,params)
 
-    solution = solve(prob)
-
-    solve_time = @elapsed solve(prob)
+    solution = solve(prob)#, RadauIIA5())
+    
+    solve_time = @elapsed solve(prob)#,  RadauIIA5())
     @show solve_time
     @show (solve_time / length(solution.t)) * 1e6
 
     integrate(solution.t, getindex.(solution.u, Int(iu0)+1))
     @btime integrate($solution.t, getindex.($solution.u, Int(iu0)+1))
 
+    N_v_course = getindex.(solution.u, Int(iN_v)+1) .- tl.pore_N0
+    N_h_course = getindex.(solution.u, Int(iN_h)+1) .- tl.pore_N0
     # Gnuplot.quitall() - plays poorly with multiplot
-    @gp "set multiplot layout 3,1"
+    @gp "set multiplot layout 3,2"
     @gp :- 1 solution.t getindex.(solution.u, Int(iu0)+1) "with lines tit 'u0'"
     @gp :- 2 solution.t getindex.(solution.u, Int(ix0_v)+1) "with lines tit 'x0_v'"
     @gp :- 3 solution.t getindex.(solution.u, Int(ix0_h)+1) "with lines tit 'x0_h'"
-
+    @gp :- 4 solution.t N_v_course "with lines tit 'N_v'"
+    @gp :- 5 solution.t N_h_course "with lines tit 'N_h'"
     # @gp :- 2 solution.t getindex.(solution.u, Int(ix1_v)+1) "with lines tit 'x0_2'"
     # @gp :- 2 solution.t getindex.(solution.u, Int(iN_h)+1) "with lines tit 'N_v'"
 
@@ -92,7 +108,7 @@ is better if you can chose where to sample the thing, like an odesolver lets you
         s = zeros(length(instances(svars)))
         t=epsilon
         
-        @btime transmembrane_params($cell_v, $cell_h, $O, $t_f, $M, $m)
+        @btime transmembrane_diffeq($d,$s,$params,t2) setup=(t2 = rand())
     end
 
     
